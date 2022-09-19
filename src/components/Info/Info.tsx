7a06bf378c4584ed33ad7094647b4b4a66f6f34a
cybervasyan <xzkto258@gmail.com>
2022-09-19 14:42:43 +0300

    feat: add info module styling

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import s from './Info.module.css'
 
 const Info = () => {
     return (
-        <div>
-            <h2>Hello</h2>
+        <div className={s.container}>
+            <h1>Welcome to the Rick and Morty Characters web application</h1>
+            <div className={s.text}>This React project will help you discover Rick and Morty characters all across the
+                series universe.
+            </div>
+            <div className={s.text}>Performed by <a href="https://github.com/Kleepers">V. Khakimov</a> and <a href="https://github.com/cybervasyan">E. Saidashev</a></div>
         </div>
     );
 };
