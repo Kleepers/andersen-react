af70b655fe9bb3bc1618944ebfbc213f954eeaea
cybervasyan <xzkto258@gmail.com>
2022-09-22 23:55:54 +0300

    feat: add PropTypes

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
 
 import {Location} from "./CardsInterfaces";
+import {deleteFavorite, selectFavorites, setFavorites} from "../../features/characterSlice";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
 
 import s from "./Cards.module.css";
+
 
 
 type Props = {
@@ -16,19 +20,57 @@ type Props = {
     }
 }
 
-const Card = ({ data }: Props) => {
+
+const Card = ({data}: Props) => {
+    const dispatch = useAppDispatch();
+
+    let isFavorite: boolean;
+    let addOrDelete = "Add to Favorites";
+    const characterId = Number(data.id);
+    const favorites = useAppSelector(selectFavorites);
+
+    if (favorites.length > 0) {
+        isFavorite = favorites.includes(characterId);
+        if(isFavorite){
+            addOrDelete = "Delete from Favorites";
+        }
+    }
+
+    function handleFavorites() {
+        if (isFavorite) {
+            dispatch(deleteFavorite(characterId))
+        } else {
+            dispatch(setFavorites(characterId))
+        }
+    }
+
     const {id, image, name, location, status} = data;
-    return <Link to={`/character/${id}`} className={s.card}>
+    return <div className={s.card}>
         <img src={image} alt="character photo"/>
         <div className={s.content}>
-            <div className={s.name}>{name}</div>
+            <Link to={`/character/${id}`} className={s.name}>{name}</Link>
+            <button onClick={handleFavorites} className={s.button}>{addOrDelete}</button>
             <div className={status.toLowerCase()}>{status}</div>
             <div>
                 <div className={s.location}>Last location</div>
                 <div>{location.name}</div>
             </div>
         </div>
-    </Link>
+    </div>
+}
+
+Card.propTypes = {
+    data: {
+        id: PropTypes.number,
+        image: PropTypes.string,
+        name: PropTypes.string,
+        location: {
+            name: PropTypes.string,
+            url: PropTypes.string
+        },
+        status: PropTypes.string,
+
+    }
 }
 
 export default Card;
